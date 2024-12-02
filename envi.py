9025820ba2f5e16afcbd440b1043c8e0c23356f2
spk2908 <shivani.kulkarni@students.iiit.ac.in>
2024-12-02 16:15:25 +0530

    partial done vrp state

--- envi.py
+++ envi.py
@@ -10,7 +10,9 @@ class Customer():
         self.assignment = 0 # 0:unassigned, 1-4: assigned to warehouse 1-4
         self.deferred = 0
         self.vehicle_id = -1 # -1: not assigned, >=0 assigned to vehicle
-        
+
+        self.cluster = None
+
 class Vehicle():
     def __init__(self, id, location, capacity):
         self.id = id
@@ -19,14 +21,74 @@ class Vehicle():
         self.current_cap = 0
         self.customers = []
         self.speed = 2
-        
-# class VRP_Agent():
-#     def __init__(self, id):
-#         self.id = id # warehouse number
-#         self.state = np.random.rand(19,) # need some function to compute the state
-#         self.feasible_actions = None
-        
-#     def get_feasible_actions(self):
+
+        self.time = 0 # time taken for the vehicle to satisfy customers in current sub-tour
+
+class CV_pair():
+    def __init__(self, customer_list, vehicle_list, c, v):
+        customer = customer_list[c]
+        vehicle = vehicle_list[v]
+
+        self.customer_id = customer.id
+        self.vehicle_id = vehicle.id
+
+        self.d = np.linalg.norm(np.array(customer.location) - np.array(vehicle.location)) # distance from loc to c
+        self.b_d_short = (self.d < customer.radius) # is d < neighborhood radius
+        self.t = np.linalg.norm(np.array(customer.location) - np.array(vehicle.location))/vehicle.speed # time taken to travel to c
+
+        self.b_t_short = (self.t <= customer.time_window[1] ) # - self.clock (current time) # is t < time window start
+        self.ngb = np.linalg.norm(vehicle.location - centroids[customer.cluster]) < radii[customer.cluster] # distance from vehicle to cluster centroid
+
+        if self.ngb:
+            self.non_d = np.inf # distance from c to nearest non-member
+            for cu in customer_list:
+                if cu.cluster != customer.cluster:
+                    self.non_d = min(self.non_d, np.linalg.norm(np.array(cu.location) - np.array(vehicle.location)))
+            self.c_left = 0
+        else:
+            self.non_d = np.inf
+            self.c_left = 0
+            for cu in customer_list:
+                if cu.cluster == customer.cluster and cu.vehicle_id == -1:
+                    self.c_left += 1
+                    break
+            
+        if self.c_left == 1:
+
+        # self.drop_far = dropped customers are vehicle.customers?
+        # self.drop_cls
+        # self.drop_long = how to calculate the distance between one point and many points?
+        self.served = len(vehicle.customers) # what if a customer's time window was missed? are they still counted as served?
+        # self.cls_dem = (vehicle.capacity - vehicle.current_cap < remaining sum of demands of cluster)
+        # self.hops = time window-wise?
+        # self.cls_tim = define feasible
+        # self.urgt = again, what is the current time for the vehicle?
+        # self.dfrac = time being used to seve c (what is this) / (vehicle.curent_cap/vehicle.capacity)
+        # self.remote = what is this?
+
+
+# not using this class
+class VRP_Agent():
+    def __init__(self, id):
+        self.depot = id # warehouse number
+        self.state = np.random.rand(19,) # need some function to compute the state
+        self.feasible_actions = None
+
+        self.customers = []
+        self.vehicles = []
+        # cluster the customers now?
+        # self.radius = r
+
+    def get_feasible_actions(self):
+        '''
+        each time do we entirely redefine the feasible actions?
+        because if v is assigned a new customer, evey VC pair with that V changes
+        and every VC pair with that C is deleted
+
+        we need to know the time a vehicle is at. Meaning, given their current sub-tour, what is the current time given their current customer list
+        when do we add return times here? because the notion of vehicles returning to depot must exist, but is not accounted for here
+        '''
+        pass
         
 
 class Environment():
@@ -40,8 +102,8 @@ class Environment():
         self.P_0max = 100
         self.vehicle_cap = 10
         self.clock = 0
-        
-    
+
+
     def _initialize_environment(self):
         num_customers = np.random.randint(200, 301)
         env_info = {
@@ -141,16 +203,86 @@ class Environment():
         # set up one agent for each warehouse
         for warehouse in self.state['warehouses']:
             warehouse['vehicles'].append(Vehicle(len(warehouse['vehicles']), warehouse['location'], self.vehicle_cap))
+
+        self.cluster_info = [{}, {}, {}, {}]
+
+        for i in range(4):
+            clocs = [customer.location for customer in self.state['customers'] if customer.assignment == i + 1 and customer.cluster == None]
+            c_idx = [customer.id for customer in self.state['customers'] if customer.assignment == i + 1 and customer.cluster == None]
+            cluster_indices, centroids, radii, rho = self._vrp_cluster_gen(clocs, 10, self.state['warehouses'][i]['location'])
+            for c in c_idx:
+                self.state['customers'][c].cluster = cluster_indices[c]
+            self.cluster_info[i]['cluster_indices'] = cluster_indices
+            self.cluster_info[i]['centroids'] = centroids
+            self.cluster_info[i]['radii'] = radii
+
+
         # initialize the state for the vrp agent
         self.vrp_states = np.random.rand(4, 19) # placeholder for actual calculation
         self.vrp_actions = []
         for i in range(4):
             self.vrp_actions.append(self._compute_feasible_actions(i))
+
+    def _vrp_cluster_gen(self, clocs, n, depot):
+        # generate distance matrix
+        # clocs = [customer.location for customer in self.state['customers'] if customer.assignment == w + 1] # and customer.cluster == None
+        ccount = len(clocs)
+        adj = np.zeros((ccount, ccount))
+        for i in range(ccount):
+            for j in range(ccount):
+                adj[i, j] = np.linalg.norm(clocs[i] - clocs[j])
+        depot_dists = np.array([np.linalg.norm(clocs[i] - depot) for i in range(ccount)])
+
+        assigned = np.zeros(ccount, dtype=bool)
+        clusters = []
+
+        while not np.all(assigned):
+            new_cluster = []
+
+            # find the closest unassigned customer to depot
+            unassigned = np.where(~assigned)[0]
+            nearest = unassigned[np.argmin(depot_dists[unassigned])]
+            new_cluster.append(nearest)
+            assigned[nearest] = True
+
+            # add n nearest unassigned customers to the cluster till no change
+            while True:
+                old_len = len(new_cluster)
+                for c in new_cluster:
+                    unassigned = np.where(~assigned)[0]
+                    nearest_neighbors = unassigned[np.argsort([adj[c, i] for i in unassigned])[:n]]
+                    for nn in nearest_neighbors:
+                        if not assigned[nn]:
+                            new_cluster.append(nn)
+                            assigned[nn] = True
+
+                if len(new_cluster) == old_len:
+                    break
+            
+            clusters.append(new_cluster)
         
+        # neighbourhood radius
+        # find cluster diameters
+        diameters = [np.max([adj[i, j] for i in c for j in c]) for c in clusters]
+        rho = np.max(diameters) / 2
+
+        # generate cluster indices for each customer
+        cluster_indices = np.zeros(ccount, dtype=int)
+        for i, c in enumerate(clusters):
+            for j in c:
+                cluster_indices[j] = i
+
+        # cluster centroids
+        centroids = [np.mean(clocs[c], axis=0) for c in clusters]
+        
+        return cluster_indices, centroids, diameters/2, rho
+
+
     def _compute_feasible_actions(self, vrp_id):
         # find the list of vehicles and customers
         vehicles = self.state['warehouses'][vrp_id]['vehicles']
-        customers = [customer for customer in self.state['customers'] if customer['assignment'] == vrp_id + 1]
+        customers = [customer for customer in self.state['customers'] if customer['assignment'] == vrp_id + 1] # customer.vehicle_id == -1
+
         # find the feasible actions
         # check distance between each vehicle and customer
         # check if the vehicle has enough capacity
@@ -163,6 +295,7 @@ class Environment():
                     <= customer.time_window[1] - self.clock):
                     feasible_actions.append((vehicle.id, customer.id))
         return feasible_actions
+    
     
     def _get_vrp_observation(self):
         self.vrp_states = np.random.rand(4, 19) # placeholder for actual calculation
@@ -182,3 +315,13 @@ class Environment():
     
     def env_step(self):
         pass
+
+    def Euclidean_CC(self, i, j):
+        customer_i = self.state['customers'][i].location
+        customer_j = self.state['customers'][j].location
+        return np.linalg.norm(np.array(customer_i.location) - np.array(customer_j.location))
+
+    def Euclidean_CV(self, c, w, v): # customer, warehouse, vehicle
+        customer = self.state['customers'][c].location
+        vehicle = self.state['warehouses'][w]['vehicles'][v].location
+        return np.linalg.norm(np.array(customer.location) - np.array(vehicle.location))
